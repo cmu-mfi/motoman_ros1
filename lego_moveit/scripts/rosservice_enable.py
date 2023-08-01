9b58bba791a943061b0155639e255ac2a7a36cf0
Shobhit <shobhita@andrew.cmu.edu>
2023-07-31 23:16:21 -0400

    added robot_enable with delay

--- lego_moveit/scripts/rosservice_enable.py
+++ lego_moveit/scripts/rosservice_enable.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
 import rospy
-from std_srvs.srv import Empty
+from std_srvs.srv import Trigger
 import time
 
 def enable_robot_service(ns):
-    rospy.wait_for_service(f'{ns}/robot_enable')
+    rospy.wait_for_service(f'/{ns}/robot_enable')
     try:
-        enable_robot = rospy.ServiceProxy('(f{ns}/robot_enable', Empty)
+        enable_robot = rospy.ServiceProxy(f'/{ns}/robot_enable', Trigger)
         resp = enable_robot()
-        return resp
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
 if __name__ == "__main__":
     rospy.init_node('enable_robot_client')
     ns = rospy.get_param('enable_robot/namespace')
-    rospy.loginfo("Requesting f{ns}/robot_enable service...")
-    time.sleep(2000)
-    print(enable_robot_service(ns))
+    rospy.loginfo(f"Requesting {ns}/robot_enable service...")
+    time.sleep(3)
+    enable_robot_service(ns)
